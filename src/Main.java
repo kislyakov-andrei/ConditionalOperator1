68d114afd558222b1a7443460cc8bd03b0705645
kislyakov-andrei <kisli@mail.ru>
2024-10-22 20:39:59 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
         // Task 1
         System.out.println("Task 1");
-        int age1 = 21;
+        int age1 = 21; // variant 1
         if (age1 >= 18) {
             System.out.println("Если возраст человека равен " + age1 + ",то он совершеннолетний");
         }
         if (age1<18) {
             System.out.println("Если возраст человека равен " + age1+ ",то он не достиг совершеннолетия, нужно немного подождать");
         }
-        int age2 = 15;
+        int age2 = 15; // variant 2
         if (age2 >= 18) {
             System.out.println("Если возраст человека равен " + age2 + ",то он совершеннолетний");
         }
@@ -19,19 +19,20 @@ public class Main {
 
         // Task 2
         System.out.println("Task 2");
-        int temperature1=2;
-        int temperature2=10;
+        int temperature1=2; // variant 1
+        int temperature2=10; // variant 2
         if (temperature1<5) {
             System.out.println("На улице "+temperature1+" градусов, нужно надеть шапку");
         }
-        else {
+        if (temperature1>=5) {
             System.out.println("На улице "+temperature1+" градусов, можно идти без шапки");
         }
         if (temperature2<5) {
             System.out.println("На улице "+temperature2+" градусов, нужно надеть шапку");
         }
-        else {
+        if (temperature2>=5) {
             System.out.println("На улице "+temperature2+" градусов, можно идти без шапки");
         }
+        // Task 3
     }
 }
